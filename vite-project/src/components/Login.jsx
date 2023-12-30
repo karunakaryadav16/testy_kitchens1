e33e77035c6af005cfb42e1bc0b254b193d603fa
karunakaryadav16 <124349022+karunakaryadav16@users.noreply.github.com>
2023-12-30 16:36:28 +0530

    Update Login.jsx

--- vite-project/src/components/Login.jsx
+++ vite-project/src/components/Login.jsx
@@ -1,52 +1,55 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { useNavigate, Navigate } from 'react-router-dom';
 import axios from 'axios';
 import { NavLink } from "react-router-dom";
 import "./Login.css";
 import Cookies from 'universal-cookie';
+
 
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
   const cookie = new Cookies();
-  const handleLogin = async () => {
-    try {
-      const response = await axios.post('http://localhost:2024/login', { username, password });
-      
-      // Set the JWT token in a cookie with the name 'jwt'
-      console.log([response])
-      cookie.set('jwt', response.data.token);
-  
-      const getCookie = cookie.get('jwt');
-      //console.log(getCookie);
-  
-     if (getCookie) {
-        navigate('/product');
-      } else {
-        console.log('Cookie is set. Redirecting to /product');
-      navigate('/');
-      }
-     }catch (error) {
-      console.error('Login error:', error);
-    }
+  console.log(username, password)
+  const renderSuccess = jwtToken => {
+    cookie.set("jwt", jwtToken)
+    navigate("/")
+  }
+
+  const handleLogin = async (e) => {
+    e.preventDefault()
+
+    // const options = {
+    //   body: JSON.stringify({ username, password })
+    // }
+
+    const response = await axios.post('http://localhost:2025/login', { username, password });
+    // const data = await response.json()
+    renderSuccess(response.data.token)
+
   };
-  
-  ;
+
+
+  const token = cookie.get("jwt")
+  if (token !== undefined) {
+    return <Navigate to="/" />
+  }
+
 
   return (
     <div className='login_con'>
-      <div>
+      <form onSubmit={handleLogin}>
         <h2>Login</h2>
         <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
         <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
 
         <div>
-          <button onClick={handleLogin}>Login</button>
+          <button type='submit'>Login</button>
           <NavLink to="/regi"> <button> Sign up</button></NavLink>
         </div>
-      </div>
-    </div>
+      </form>
+    </div >
   );
 };
 
