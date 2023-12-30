ea998d51ef8ec2d10276be06ba3f6991c6b08d4f
karunakaryadav16 <124349022+karunakaryadav16@users.noreply.github.com>
2023-12-30 16:38:07 +0530

    Update ProtectedRoute.jsx

--- vite-project/src/components/ProtectedRoute.jsx
+++ vite-project/src/components/ProtectedRoute.jsx
@@ -1,3 +1,51 @@
+// /*import React from 'react';
+// import { Route, Navigate } from 'react-router-dom';
+// import Cookies from 'universal-cookie';
+// import Movies from './Movies';
+
+// const ProtectedRoute = ({ element, ...rest }) => {
+//   const cookies = new Cookies();
+//   const token = cookies.get('jwt');
+
+//   return token ? (
+//     <Route {...rest} element={<Movies/>} />
+//   ) : (
+//     <Navigate to="/"/>
+//   );
+// };
+
+// export default ProtectedRoute;
+// */
+
+// import { Navigate } from 'react-router-dom';
+
+// //import { Route} from 'react-router-dom'
+// import Cookies from 'universal-cookie';
+
+
+// const ProtectedRoute = ({children}) => {
+//  // const navigate = useNavigate();
+//   const cookie = new Cookies();
+
+
+//   const token = cookie.get('jwt')
+//   console.log(token)//////read
+//   if(token){
+//     return children
+//   }else{
+//     return <Navigate to="/" />
+    
+//   }
+// }
+
+// export default ProtectedRoute
+
+
+
+
+// //
+
+/* eslint-disable react/prop-types */
 /*import React from 'react';
 import { Route, Navigate } from 'react-router-dom';
 import Cookies from 'universal-cookie';
@@ -23,34 +71,21 @@ import { Navigate } from 'react-router-dom';
 import Cookies from 'universal-cookie';
 
 
-const ProtectedRoute = ({children}) => {
- // const navigate = useNavigate();
+const ProtectedRoute = ({ children }) => {
+  // const navigate = useNavigate();
   const cookie = new Cookies();
 
 
   const token = cookie.get('jwt')
-  console.log(token)//////read
-  if(token){
-    return children
-  }else{
-    return <Navigate to="/" />
-    
-  }
+  // console.log(token)//////read
+
+  if (token === undefined) {
+    return <Navigate to="/login" />
+  } return children
+
 }
 
 export default ProtectedRoute
-
-
-
-
-//
-
-
-
-
-
-
-
 
 
 
